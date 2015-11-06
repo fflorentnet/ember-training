5fb27145d99b6d1d5151a8a5333a555e6000d515
Baptiste Meurant <baptiste.meurant@gmail.com>
2015-11-06 17:29:52 +0100

    Routing - 2 - Models

---
+++ app/models/comic.js
@@ -0,0 +1,9 @@
+import EmberObject from '@ember/object';
+
+export default EmberObject.extend({
+  slug: '',
+  title: '',
+  scriptwriter: '',
+  illustrator: '',
+  publisher: ''
+});
--- app/routes/comics.js
+++ app/routes/comics.js
@@ -1,12 +1,35 @@
 import Route from '@ember/routing/route';
+import Comic from 'ember-training/models/comic';
+
+const blackSad = Comic.create({
+  slug: 'blacksad',
+  title: 'Blacksad',
+  scriptwriter: 'Juan Diaz Canales',
+  illustrator: 'Juanjo Guarnido',
+  publisher: 'Dargaud'
+});
+
+const calvinAndHobbes = Comic.create({
+  slug: 'calvin-and-hobbes',
+  title: 'Calvin and Hobbes',
+  scriptwriter: 'Bill Watterson',
+  illustrator: 'Bill Watterson',
+  publisher: 'Andrews McMeel Publishing'
+});
+
+const akira = Comic.create({
+  slug: 'akira',
+  title: 'Akira',
+  scriptwriter: 'Katsuhiro Ôtomo',
+  illustrator: 'Katsuhiro Ôtomo',
+  publisher: 'Epic Comics'
+});
+
+const comics = [blackSad, calvinAndHobbes, akira];
 
 export default Route.extend({
 
   model() {
-    // WARN : SHOULD NOT BE DONE : We should not affect anything to window but 
-    // for the exercice, we want to access to comic from console today
-    window.comics = [{title: "Blacksad"}, {title: "Calvin and Hobbes", scriptwriter:"Bill Watterson"}];
-
     return comics;
   }
 });
