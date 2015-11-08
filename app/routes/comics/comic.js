a73c4badd50a37ae9b70672605e0e16d8ca59a88
Baptiste Meurant <baptiste.meurant@gmail.com>
2015-11-08 12:45:39 +0100

    Routing - 4 - Dynamic segments

--- app/routes/comics/comic.js
+++ app/routes/comics/comic.js
@@ -1,4 +1,7 @@
-import Ember from 'ember';
+import Route from '@ember/routing/route';
 
-export default Ember.Route.extend({
+export default Route.extend({
+  model (params) {
+    return this.modelFor('comics').findBy('slug', params.comic_slug);
+  }
 });
