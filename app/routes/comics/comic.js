d5ed741b1cab55d1539e4079bb4815cbca59be23
Baptiste Meurant <baptiste.meurant@gmail.com>
2015-11-09 00:02:37 +0100

    Routing - 6 - Implicit routes

--- app/routes/comics/comic.js
+++ app/routes/comics/comic.js
@@ -2,7 +2,13 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model (params) {
-    return this.modelFor('comics').findBy('slug', params.comic_slug);
+    let askedModel = this.modelFor('comics').findBy('slug', params.comic_slug);
+
+    if (askedModel === undefined) {
+      throw new Error("No comic found with slug: " + params.comic_slug);
+    }
+
+    return askedModel;
   },
   serialize: function(model) {
     return {
