ca2a40c0019ff3f1efcd32a653fcf1b12defa270
Baptiste Meurant <baptiste.meurant@gmail.com>
2016-04-04 21:57:00 +0200

    Components - 5 - lifecyle

--- app/components/image-cover.js
+++ app/components/image-cover.js
@@ -6,6 +6,25 @@ export default Component.extend({
   classNames: 'cover',
   attributeBindings: 'src',
   src: computed('name', function () {
-    return `/assets/images/comics/covers/${this.get('name')}.jpg`;
-  })
+    return this.getImagePath(this.get('name'));
+  }),
+
+  getImagePath(name) {
+    return `/assets/images/comics/covers/${name}.jpg`;
+  },
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.$().on('error', () => {
+      return this.onError();
+    });
+  },
+
+  willDestroyElement(){
+    this.$().off('error');
+  },
+
+  onError() {
+    this.$().attr('src', this.getImagePath('default'));
+  }
 });
