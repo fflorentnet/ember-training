2f0f47d78d97894ca1530ef7eeb86a9253acc356
Baptiste Meurant <baptiste.meurant@gmail.com>
2016-04-18 19:42:48 +0200

    Ember Data 2 - Mirage

--- app/routes/comics.js
+++ app/routes/comics.js
@@ -1,37 +1,7 @@
 import Route from '@ember/routing/route';
 
-const blackSad = {
-  slug: 'blacksad',
-  title: 'Blacksad',
-  scriptwriter: 'Juan Diaz Canales',
-  illustrator: 'Juanjo Guarnido',
-  publisher: 'Dargaud'
-};
-
-const calvinAndHobbes = {
-  slug: 'calvin-and-hobbes',
-  title: 'Calvin and Hobbes',
-  scriptwriter: 'Bill Watterson',
-  illustrator: 'Bill Watterson',
-  publisher: 'Andrews McMeel Publishing'
-};
-
-const akira = {
-  slug: 'akira',
-  title: 'Akira',
-  scriptwriter: 'Katsuhiro Ôtomo',
-  illustrator: 'Katsuhiro Ôtomo',
-  publisher: 'Epic Comics'
-};
-
 export default Route.extend({
-  init() {
-    this._super(...arguments);
-    this.store.createRecord('comic', akira);
-    this.store.createRecord('comic', blackSad);
-    this.store.createRecord('comic', calvinAndHobbes);
-  },
   model () {
-    return this.store.peekAll('comic');
+    return this.store.findAll('comic');
   }
 });
