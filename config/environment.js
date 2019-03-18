fca293f0ad9c0510c7918bf70de361403e4eb526
Florent <florent.foure@worldline.com>
2019-03-18 11:08:05 +0100

    fix(run): correction de lancement de l'application

--- config/environment.js
+++ config/environment.js
@@ -34,10 +34,11 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
 
-    ENV['ember-cli-mirage'] = {
-      enabled: false
-    };
-    ENV.host =  'http://localhost:3000';
+    //TODO: a tester
+    //ENV['ember-cli-mirage'] = {
+    //  enabled: false
+    //};
+    ENV.host =  'http://localhost:4200';
   }
 
   if (environment === 'test') {
