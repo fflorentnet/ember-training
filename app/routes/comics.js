e298d97f2f372e72cf6d990fa3354b430e5d0c74
Florent <florent.foure@worldline.com>
2019-03-18 11:17:51 +0100

    chore(package): ajout de decorators

--- app/routes/comics.js
+++ app/routes/comics.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember-decorators/service';
 
-export default Route.extend({
+export default class ComicsRoute extends Route {
+
+  @service store;
+
   model () {
     return this.store.findAll('comic');
   }
-});
+};
